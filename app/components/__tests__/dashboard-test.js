00fa4f4c3152fb98542d1de09f1cf53aace8f2bb
Eric Tillberg <eric.tillberg@gmail.com>
2016-12-03 20:15:42 -0500

    wip

--- app/components/__tests__/dashboard-test.js
+++ app/components/__tests__/dashboard-test.js
@@ -1,15 +1,29 @@
 import Dashboard from '../dashboard';
+import Video from '../video';
 
 describe('Dashboard', () => {
+  let dashboard, getUserMedia, video;
+
+  beforeEach(() => {
+    dashboard = mount(<Dashboard />);
+    getUserMedia = sinon.spy();
+    video = dashboard.find(Video);
+  });
+
   describe('Initialization', () => {
 
   });
 
   describe('Layout', () => {
-
+    it('has a video component', () => {
+      expect(video).to.have.length(1);
+    });
   });
 
   describe('Interaction', () => {
-
+    it('record function calls getUserMedia', () => {
+      dashboard.instance().record();
+      expect(getUserMedia.called).to.eql(true);
+    });
   });
 });
--- app/components/dashboard.jsx
+++ app/components/dashboard.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import Video from './video';
 
 export default class Dashbaord extends React.Component {
+  constructor() {
+    super();
+    this.record = this.record.bind(this);
+  }
+
+  record() {
+    console.log(navigator)
+    navigator.mediaDevices.getUserMedia({video: true, audio: true}).then(stream => {
+console.log('hallo')
+    });
+  }
+
   render () {
     return (
       <div>
-        <Video record={record} />
+        <Video record={this.record} />
       </div>
     )
   }
